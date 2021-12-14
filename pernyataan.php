481899c36f20ba4db6375a86a8c91062a75bc9b1
DillaAnjani <90698905+DillaAnjani@users.noreply.github.com>
2021-12-14 10:47:29 +0700

    post test 2

---
+++ pernyataan.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>POST TEST BENAR SALAH</title>
+</head>
+<body>
+<?php
+$color = "red";
+switch ($color){
+  case "red":
+   echo "BENAR";
+    break;
+    case "green":
+      echo  "SALAH";
+        break;
+
+};
+?>
+</body>
+</html>
